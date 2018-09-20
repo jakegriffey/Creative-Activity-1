c8b08745a20fc026985fff37ef8a4da0b6d3b51a
Jake Griffey <jakegriffey@gmail.com>
2018-09-20 04:40:41 +0000

    added score screen and changed score logic

--- moviequiz.js
+++ moviequiz.js
@@ -1,13 +1,13 @@
-let movieQuotes = ["I see in your eyes the same fear that would take the heart of me! A day may come when the courage of men fails, when we forsake our friends and break all bonds of fellowship. But it is not this day.",
-                       "We’ve all got both light and darkness inside us. What matters is the part we choose to act on.",
-                       "Every time I walked away from something I wanted to forget, I told myself it was for a cause that I believed in. A cause that was worth it. Without that, we’re lost.",
-                       "We used to look up at the sky and wonder at our place in the stars. Now we just look down, and worry about our place in the dirt.",
-                       "Do you know why you're afraid when you're alone? I do. I do.",
-                       "Do I really look like a guy with a plan? You know what I am? I'm a dog chasing cars. I wouldn't know what to do with one if I caught it!",
-                       "What is the most resilient parasite? Bacteria? A virus? An intestinal worm? An idea. Resilient... highly contagious. Once an idea has taken hold of the brain it's almost impossible to eradicate",
-                       "That's all it is. You just begin. You do the math. You solve one problem... and you solve the next one... and then the next. And If you solve enough problems, you get to come home.",
-                       "Look, I'm not the one with the problem, okay? It's the world that seems to have a problem with ME!",
-                       "I don't like sand. It's coarse and rough and irritating and it gets everywhere."];
+let movieQuotes = ["\"I see in your eyes the same fear that would take the heart of me! A day may come when the courage of men fails, when we forsake our friends and break all bonds of fellowship. But it is not this day.\"",
+                       "\"We’ve all got both light and darkness inside us. What matters is the part we choose to act on.\"",
+                       "\"Every time I walked away from something I wanted to forget, I told myself it was for a cause that I believed in. A cause that was worth it. Without that, we’re lost.\"",
+                       "\"We used to look up at the sky and wonder at our place in the stars. Now we just look down, and worry about our place in the dirt.\"",
+                       "\"Do you know why you're afraid when you're alone? I do. I do.",
+                       "\"Do I really look like a guy with a plan? You know what I am? I'm a dog chasing cars. I wouldn't know what to do with one if I caught it!\"",
+                       "\"What is the most resilient parasite? Bacteria? A virus? An intestinal worm? An idea. Resilient... highly contagious. Once an idea has taken hold of the brain it's almost impossible to eradicate.\"",
+                       "\"That's all it is. You just begin. You do the math. You solve one problem... and you solve the next one... and then the next. And If you solve enough problems, you get to come home.\"",
+                       "\"Look, I'm not the one with the problem, okay? It's the world that seems to have a problem with ME!\"",
+                       "\"I don't like sand. It's coarse and rough and irritating and it gets everywhere.\""];
 
 let questionAnswers = [["The Lord of the Rings", "Braveheart", "A Few Good Men", "Gone With the Wind"],
                        ["The Lego Movie", "The Giver", "Harry Potter and the Order of the Phoenix", "Star Wars: The Last Jedi"],
@@ -24,7 +24,8 @@ let correctChoices = [0, 2, 3, 2, 0, 1, 3, 1, 2, 0];
 
 let currentQuestion = -1;
 
-var points = 0;
+let points = 0;
+let correctAnswers = 0;
 
 function checkAnswer(element) {
     let chosenAnswer = element.id //Get the id of the button that was clicked
@@ -40,13 +41,22 @@ function checkAnswer(element) {
         chosenAnswer = 3;
     }
     
+    document.getElementById("AnswerA").disabled = true;
+    document.getElementById("AnswerB").disabled = true;
+    document.getElementById("AnswerC").disabled = true;
+    document.getElementById("AnswerD").disabled = true;
+    document.getElementById("CheckButton").disabled = true;
+        
+    
     //Check the answer
     let answerIndicator = document.getElementById("AnswerIndicator");
     if(chosenAnswer == correctChoices[currentQuestion]) {
         //show that it is correct
         answerIndicator.innerHTML = "Correct!";
-        answerIndicator.style.color = "green";
+        answerIndicator.style.color = "lightgreen";
         points++;
+        
+        correctAnswers += 1;
         returnValue = true;
     } else {
         //show that it was incorrect
@@ -65,10 +75,8 @@ function moveToNextQuestion() {
         currentQuestion = currentQuestion + 1;
     
         //Update header and question
-        document.getElementById("QuestionHeader").innerHTML = "Question " + (currentQuestion + 1);
+        document.getElementById("QuestionHeader").innerHTML = "Quote " + (currentQuestion + 1);
         document.getElementById("Question").innerHTML = movieQuotes[currentQuestion];
-        
-        
     
         //Update Answers
         document.getElementById("AnswerA").innerHTML = questionAnswers[currentQuestion][0];
@@ -76,14 +84,28 @@ function moveToNextQuestion() {
         document.getElementById("AnswerC").innerHTML = questionAnswers[currentQuestion][2];
         document.getElementById("AnswerD").innerHTML = questionAnswers[currentQuestion][3];
         
+        document.getElementById("AnswerA").disabled = false;
+        document.getElementById("AnswerB").disabled = false;
+        document.getElementById("AnswerC").disabled = false;
+        document.getElementById("AnswerD").disabled = false;
+        document.getElementById("CheckButton").disabled = false;
+        
     
         //Remove Answer Indicator
         document.getElementById("AnswerIndicator").innerHTML = "";
     } else {
         
         //just for debugging to make sure that it's outputing the right number of points.
-        alert(points);
+        //alert(points);
         //move to final screen
+        
+        let score = points;
+        if(score > 0) {
+            score += "0";
+        }
+        let scoreURL = "./end.html?score=" + score.toString();
+        console.log(scoreURL);
+        document.getElementById("SeeScore").innerHTML = "Click <a href = " + scoreURL + ">Here</a> To See Your Score";
     }
     
     
@@ -91,9 +113,9 @@ function moveToNextQuestion() {
 
 function handleAnswer(f)
 {
-    checkAnswer(document.getElementById(f.answer.value))
+    checkAnswer(document.getElementById(f.answer.value));
     //unconditionally moving to next question
-    setTimeout(moveToNextQuestion, 2000);
+    setTimeout(moveToNextQuestion, 1000);
     //This code clears the answer choice when you answer incorrectly or are going to move to the next question  
     var radio=document.getElementById(f.answer.value.replace("Answer", ""));
     radio.checked = false;
